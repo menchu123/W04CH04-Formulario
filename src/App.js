c0e0cc119a0822c90b13b20e9839f6f52dbcdebd
Menchu MV <mcmvillalobos@gmail.com>
2021-10-22 19:58:04 +0200

    Added Access data fields

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <section className="container d-flex justify-content-center align-items-center mh-100">
       <form className="form-container">
-        <div className="form-group">
+        {/* <div className="form-group">
           <label htmlFor="form_name">Name</label>
           <input
             type="text"
@@ -42,6 +42,44 @@ function App() {
             ></input>
           </div>
         </div>
+        <div className="form-group">
+          <label htmlFor="form_email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            pattern="[^ @]*@[^ @]*"
+            placeholder="Enter your email"
+          ></input>
+        </div> */}
+        <div className="form-group">
+          <label htmlFor="form_username">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="form_username"
+            placeholder="Enter username"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="form_password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="form_password"
+            placeholder="Enter your password"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="form_password">Repeat your password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="form_password"
+            placeholder="Repeat your password"
+          ></input>
+        </div>
+
         <section className="navigation d-flex justify-content-between">
           <button class="btn btn-primary mt-4">Go back</button>
           <button class="btn btn-primary mt-4">Next</button>
