6985c5de15565324d3a3db379786b2b24c86ff24
Menchu MV <mcmvillalobos@gmail.com>
2021-10-22 19:34:46 +0200

    Added Personaldata fields

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-   
+    <section className="form-container container">
+      <form>
+        <div className="form-group">
+          <label htmlFor="form_name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="form_name"
+            placeholder="Enter name"
+          ></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="form_lastname">Last Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="form_lastname"
+            placeholder="Enter your last name"
+          ></input>
+        </div>
+        <div className="row">
+          <div className="form-group col-10">
+            <label htmlFor="form_birthdate">Date of Birth</label>
+            <input
+              type="date"
+              className="form-control"
+              id="form_birthdate"
+            ></input>
+          </div>
+          <div className="form-group col-2">
+            <label htmlFor="age">Age</label>
+            <input
+              type="text"
+              class="form-control"
+              id="age"
+              value="27"
+              readOnly
+            ></input>
+          </div>
+        </div>
+      </form>
+    </section>
   );
 }
 
