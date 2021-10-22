d3623b64aae2dba06daddaaaf10e8f955b7a830b
Menchu MV <mcmvillalobos@gmail.com>
2021-10-22 20:29:45 +0200

    PersonalData component html applied

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,1 +1,72 @@
-const Form = () => {};
+import PersonalData from "./PersonalData/PersonalData";
+
+const Form = () => {
+  return (
+    <form className="form-container">
+      <PersonalData />
+      <div className="form-group">
+        <label htmlFor="form_username">Username</label>
+        <input
+          type="text"
+          className="form-control"
+          id="form_username"
+          placeholder="Enter username"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="form_password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="form_password"
+          placeholder="Enter your password"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="form_password">Repeat your password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="form_password"
+          placeholder="Repeat your password"
+        ></input>
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="form_username">Username</label>
+        <input
+          type="text"
+          className="form-control"
+          id="form_username"
+          placeholder="Enter username"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="form_password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="form_password"
+          placeholder="Enter your password"
+        ></input>
+      </div>
+      <div class="custom-control custom-checkbox mt-2">
+        <input
+          type="checkbox"
+          class="custom-control-input"
+          id="remember-password"
+        ></input>
+        <label class="custom-control-label m-2" for="remember-password">
+          Remember password
+        </label>
+      </div>
+
+      <section className="navigation d-flex justify-content-between">
+        <button class="btn btn-primary mt-4">Go back</button>
+        <button class="btn btn-primary mt-4">Next</button>
+      </section>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/components/Form/PersonalData/PersonalData.js
@@ -0,0 +1,56 @@
+const PersonalData = () => {
+  return (
+    <>
+      <div className="form-group">
+        <label htmlFor="form_name">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="form_name"
+          placeholder="Enter name"
+        ></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="form_lastname">Last Name</label>
+        <input
+          type="text"
+          className="form-control"
+          id="form_lastname"
+          placeholder="Enter your last name"
+        ></input>
+      </div>
+      <div className="row">
+        <div className="form-group col-10">
+          <label htmlFor="form_birthdate">Date of Birth</label>
+          <input
+            type="date"
+            className="form-control"
+            id="form_birthdate"
+          ></input>
+        </div>
+        <div className="form-group col-2">
+          <label htmlFor="age">Age</label>
+          <input
+            type="text"
+            class="form-control"
+            id="age"
+            value="27"
+            readOnly
+          ></input>
+        </div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="form_email">Email</label>
+        <input
+          type="email"
+          className="form-control"
+          name="email"
+          pattern="[^ @]*@[^ @]*"
+          placeholder="Enter your email"
+        ></input>
+      </div>
+    </>
+  );
+};
+
+export default PersonalData;
