25be35f9c83716a82fa6696ffaaaa39ed90d063d
Menchu MV <mcmvillalobos@gmail.com>
2021-10-22 20:31:23 +0200

    AccessData component html applied

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,37 +1,11 @@
+import AccessData from "./AccessData/AccessData";
 import PersonalData from "./PersonalData/PersonalData";
 
 const Form = () => {
   return (
     <form className="form-container">
       <PersonalData />
-      <div className="form-group">
-        <label htmlFor="form_username">Username</label>
-        <input
-          type="text"
-          className="form-control"
-          id="form_username"
-          placeholder="Enter username"
-        ></input>
-      </div>
-      <div className="form-group">
-        <label htmlFor="form_password">Password</label>
-        <input
-          type="password"
-          className="form-control"
-          id="form_password"
-          placeholder="Enter your password"
-        ></input>
-      </div>
-      <div className="form-group">
-        <label htmlFor="form_password">Repeat your password</label>
-        <input
-          type="password"
-          className="form-control"
-          id="form_password"
-          placeholder="Repeat your password"
-        ></input>
-      </div>
-
+      <AccessData />
       <div className="form-group">
         <label htmlFor="form_username">Username</label>
         <input
